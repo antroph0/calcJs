5b74561ac0285add9975241d5c44706346f66cd9
Jorge FR <64752270+antroph0@users.noreply.github.com>
2021-11-25 00:35:39 -0300

    primer avance

---
+++ js/function.js
@@ -0,0 +1,54 @@
+function init(){
+    //vars 
+    var equal = document.getElementById("equal"); 
+    var result = document.getElementById("result");
+    var add = documento.getElementById("add");
+    var multiplication = document.getElementById("multiplication");
+    var division = document.getElementById("division");
+    var substract = document.getElementById("substract");
+   
+    var zero = document.getElementById("zero");
+    var nine = documento.getElementById("nine");
+    var eigth = documento.getElementById("eigth");
+    var seven = documento.getElementById("seven");
+    var six = document.getElementById("six");
+    var five = document.getElementById("five"); 
+    var four = document.getElementById("four");
+    var three = document.getElementById("three"); 
+    var two = document.getElementById("two");
+    var one = document.getElementById("one"); 
+
+    //Events
+
+    one.onclick = function(e){
+        result.TextContent = result.TextContent + "1"; 
+    }
+    two.onclick = function(e){
+        result.TextContent = result.TextContent + "2"; 
+    }
+    three.onclick = function(e){
+        result.TextContent = result.TextContent + "3"; 
+    }
+    three.onclick = function(e){
+        result.TextContent = result.TextContent + "4"; 
+    }
+    three.onclick = function(e){
+        result.TextContent = result.TextContent + "5"; 
+    }
+    three.onclick = function(e){
+        result.TextContent = result.TextContent + "6"; 
+    }
+    three.onclick = function(e){
+        result.TextContent = result.TextContent + "7"; 
+    }
+    three.onclick = function(e){
+        result.TextContent = result.TextContent + "8"; 
+    }
+    three.onclick = function(e){
+        result.TextContent = result.TextContent + "9"; 
+    }
+    three.onclick = function(e){
+        result.TextContent = result.TextContent + "0"; 
+    }
+    
+}
